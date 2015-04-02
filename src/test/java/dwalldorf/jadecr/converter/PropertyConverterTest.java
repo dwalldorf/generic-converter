0381d4f6e1e6d1f4145a75ad646eadea7d7f92c3
Daniel Walldorf <entekmedia@gmail.com>
2015-04-02 23:13:21 +0200

    Tests for PropertyConverter

---
+++ src/test/java/dwalldorf/jadecr/converter/PropertyConverterTest.java
@@ -0,0 +1,113 @@
+/*
+ jadecR
+ Copyright (C) 2015  Daniel Walldorf
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package dwalldorf.jadecr.converter;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import sample.dto.MessageDto;
+import sample.dto.UserDto;
+import sample.model.Message;
+import sample.model.User;
+
+public class PropertyConverterTest extends TestCase {
+
+  private Converter propertyConverter = new PropertyConverter();
+
+  @Test
+  public void testConvertToDto() {
+    User user = mockUser();
+    Message message = mockMessage(user);
+
+    MessageDto messageDto = (MessageDto) propertyConverter.convert(message);
+    UserDto userDto = messageDto.getUser();
+
+    assertNotNull(messageDto);
+    assertEquals(message.getId(), messageDto.getId());
+    assertEquals(message.getTitle(), messageDto.getTitle());
+    assertEquals(message.getMessage(), messageDto.getMessage());
+
+    assertNotNull(userDto);
+    assertEquals(user.getId(), userDto.getId());
+    assertEquals(user.getUsername(), userDto.getUsername());
+    assertEquals(user.getPassword(), userDto.getPassword());
+    assertEquals(user.getEmail(), userDto.getEmail());
+  }
+
+  @Test
+  public void testConvertToEntity() {
+    UserDto userDto = mockUserDto();
+    MessageDto messageDto = mockMessageDto(userDto);
+
+    Message message = (Message) propertyConverter.convert(messageDto);
+    User user = message.getUser();
+
+    assertNotNull(messageDto);
+    assertEquals(messageDto.getId(), message.getId());
+    assertEquals(messageDto.getTitle(), message.getTitle());
+    assertEquals(messageDto.getMessage(), message.getMessage());
+
+    assertNotNull(user);
+    assertEquals(userDto.getId(), user.getId());
+    assertEquals(userDto.getUsername(), user.getUsername());
+    assertEquals(userDto.getPassword(), user.getPassword());
+    assertEquals(userDto.getEmail(), user.getEmail());
+  }
+
+  private User mockUser() {
+    User user = new User();
+    user.setId(1L);
+    user.setUsername("testuser");
+    user.setEmail("mail@host.tld");
+    user.setPassword("password");
+    user.setNoMatchingFieldInDto("this field does not exist in dto");
+
+    return user;
+  }
+
+  private Message mockMessage(final User user) {
+    Message message = new Message();
+    message.setId(42L);
+    message.setTitle("title");
+    message.setMessage("message");
+    message.setUser(user);
+
+    return message;
+  }
+
+  private UserDto mockUserDto() {
+    UserDto userDto = new UserDto();
+    userDto.setId(1L);
+    userDto.setUsername("testuser");
+    userDto.setEmail("mail@host.tld");
+    userDto.setPassword("password");
+
+    return userDto;
+  }
+
+  private MessageDto mockMessageDto(final UserDto userDto) {
+    MessageDto messageDto = new MessageDto();
+    messageDto.setId(42L);
+    messageDto.setTitle("title");
+    messageDto.setMessage("messageDto");
+    messageDto.setUser(userDto);
+
+    return messageDto;
+  }
+
+}
