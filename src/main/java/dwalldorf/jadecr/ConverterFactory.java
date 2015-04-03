5caa67378a285f62df780a8d9b08822665bd096c
Daniel Walldorf <entekmedia@gmail.com>
2015-04-03 18:53:58 +0200

    refactored ConverterFactory

--- src/main/java/dwalldorf/jadecr/ConverterFactory.java
+++ src/main/java/dwalldorf/jadecr/ConverterFactory.java
@@ -3,33 +3,39 @@ package dwalldorf.jadecr;
 import dwalldorf.jadecr.converter.Converter;
 import dwalldorf.jadecr.converter.GetterSetterConverter;
 import dwalldorf.jadecr.converter.PropertyConverter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Factory to configure the converter and obtain an instance of it.
  */
 public class ConverterFactory {
 
-  private static Converter instance = null;
+  private final static Map<ConverterType, Converter> instanceMap = new HashMap<>();
 
   private static ConverterType converterType = ConverterType.GETTER_SETTER;
 
   public static Converter getInstance() {
-    if (instance == null) {
-      switch (converterType) {
-        case GETTER_SETTER:
-          instance = new GetterSetterConverter();
-          break;
-        case CLASS_MEMBER:
-          instance = new PropertyConverter();
-          break;
-      }
+    if (!instanceMap.containsKey(converterType)) {
+      instanceMap.put(converterType, createInstance(converterType));
     }
 
-    return instance;
+    return instanceMap.get(converterType);
   }
 
   public static void configureType(final ConverterType type) {
     converterType = type;
+  }
+
+  private static Converter createInstance(ConverterType type) {
+    switch (type) {
+      case GETTER_SETTER:
+        return new GetterSetterConverter();
+      case CLASS_MEMBER:
+        return new PropertyConverter();
+      default:
+        return null;
+    }
   }
 
 }
